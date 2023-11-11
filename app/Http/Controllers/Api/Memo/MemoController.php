9ada917a8f522e57decbfb00c0f5af8d581a2e84
sneko <supernekocat31@gmail.com>
2023-11-11 11:16:00 +0900

    step.3 REFACTORING テストがGREENの範囲で修正

--- app/Http/Controllers/Api/Memo/MemoController.php
+++ app/Http/Controllers/Api/Memo/MemoController.php
@@ -5,24 +5,13 @@ namespace App\Http\Controllers\Api\Memo;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use App\Http\Requests\MemoRequest;
 
 class MemoController extends Controller
 {
     
-    public function store(Request $request): JsonResponse
+    public function store(MemoRequest $request): JsonResponse
     {
-        $title = $request->get('title');
-        $content = $request->get('content');
- 
-        if ($title === null || $content === null) {
-            return response()->json([
-                'message' => '登録失敗',
-            ]);
-        }
- 
-        // メモの作成処理を実装
-        // ここでは、まだDBに接続しないため、処理を省略しています
- 
         return response()->json([
             'message' => '登録完了',
         ]);
---
+++ app/Http/Requests/MemoRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class MemoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required',
+            'content' => 'required'
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        $response = ['message' => '登録失敗'];
+        // 422 Unprocessable entity error
+        throw new HttpResponseException(response()->json($response, 422));
+    }
+}
